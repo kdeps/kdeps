557e63efb6651180857aeb724b5b7271356533d4
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2024-09-07 15:36:35 +0200

    aded config and workflow pkl file enforcer

--- features/enforcer/config.feature
+++ features/enforcer/config.feature
@@ -2,30 +2,27 @@ Feature: Config enforcer
   Background:
     Given the home directory is "/home/user"
     And the current directory is "/current/directory"
+    And we have a blank config file
 
   Scenario: Configuration file exists in the current directory with an amends line
-    Given we have a blank config file
-    And it have a config amends line on top of the file
+    Given it have a config amends line on top of the file
     And it have a "kdeps.com" amends url line on top of the file
     When a file ".kdeps.pkl" exists in the current directory
     Then it is a valid configuration file
 
   Scenario: Configuration file exists in the current directory without an amends line
-    Given we have a blank config file
-    And it does not have a config amends line on top of the file
+    Given it does not have a config amends line on top of the file
     When a file ".kdeps.pkl" exists in the current directory
-    Then it is a invalid configuration file
+    Then it is an invalid configuration file
 
   Scenario: Configuration file exists in the current directory with an amends line
-    Given we have a blank config file
-    And it have a config amends line on top of the file
+    Given it have a config amends line on top of the file
     And it have a "domain.com" amends url line on top of the file
     When a file ".kdeps.pkl" exists in the current directory
-    Then it is a invalid configuration file
+    Then it is an invalid configuration file
 
   Scenario: Configuration file exists in the current directory with an amends line and different pkl file
-    Given we have a blank config file
-    And it have a config amends line on top of the file
+    Given it have a config amends line on top of the file
     And it have a "kdeps.com" amends url line on top of the file
-    When a file ".kdeps.pkl" exists in the current directory
-    Then it is a invalid configuration file
+    When a file ".kdeps1.pkl" exists in the current directory
+    Then it is an invalid configuration file
--- pkg/enforcer/enforcer_test.go
+++ pkg/enforcer/enforcer_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"kdeps/pkg/cfg"
+	"kdeps/pkg/evaluator"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -19,9 +20,11 @@ var (
 	currentDirPath        string
 	systemConfiguration   *kdeps.Kdeps
 	fileThatExist         string
+	agentPath             string
 	doc                   string
 	schemaVersionFilePath = "../../SCHEMA_VERSION"
-	amendsLine            = `amends "package://schema.kdeps.com/core@0.0.26#/Kdeps.pkl"`
+	workflowAmendsLine    = `amends "package://schema.kdeps.com/core@0.0.26#/Workflow.pkl"`
+	configAmendsLine      = `amends "package://schema.kdeps.com/core@0.0.26#/Kdeps.pkl"`
 	configValues          = `
 runMode = "docker"
 dockerGPU = "cpu"
@@ -37,41 +40,65 @@ llmSettings {
   modelFile = null
 }
 `
+	workflowValues = `
+settings {
+  runTimeout = 15.min
+  interactiveOnMissingValues = false
+  llmSettings {
+    llmAPIKeys {
+      openai_api_key = null
+      mistral_api_key = null
+      huggingface_api_token = null
+      groq_api_key = null
+    }
+    llmFallbackBackend = "ollama"
+    llmFallbackModel = "llama3.1"
+    modelFile = null
+  }
+  apiServerMode = false
+  apiServerSettings {
+    serverPort = 3000
+    routes {
+      new {
+	path = "/api"
+	methods {
+	  "POST"
+	}
+	requestParams = "ENV:API_SERVER_REQUEST_PARAMS"
+	request = "ENV:API_SERVER_REQUEST"
+	response = "ENV:API_SERVER_RESPONSE"
+      }
+    }
+  }
+}
+workflows {}
+args = null
+`
 	testingT *testing.T
 )
 
 func TestFeatures(t *testing.T) {
 	suite := godog.TestSuite{
 		ScenarioInitializer: func(ctx *godog.ScenarioContext) {
-			ctx.Step(`^we have a blank config file$`, weHaveABlankConfigFile)
-			ctx.Step(`^a file "([^"]*)" also exists in the "([^"]*)" folder$`, aFileAlsoExistsInTheFolder)
+			// Configuration steps
+			ctx.Step(`^we have a blank config file$`, weHaveABlankFile)
 			ctx.Step(`^a file "([^"]*)" exists in the current directory$`, aFileExistsInTheCurrentDirectory)
-			ctx.Step(`^a file "([^"]*)" exists in the "([^"]*)" folder$`, aFileExistsInTheFolder)
 			ctx.Step(`^a system configuration is defined$`, aSystemConfigurationIsDefined)
-			ctx.Step(`^all resources have a valid amends line$`, allResourcesHaveAValidAmendsLine)
-			ctx.Step(`^all resources have a valid domain$`, allResourcesHaveAValidDomain)
-			ctx.Step(`^all resources have an amends line$`, allResourcesHaveAnAmendsLine)
-			ctx.Step(`^an agent folder "([^"]*)" exists in the current directory$`, anAgentFolderExistsInTheCurrentDirectory)
-			ctx.Step(`^"([^"]*)" does not have an amends line$`, doesNotHaveAnAmendsLine)
-			ctx.Step(`^"([^"]*)" have a "([^"]*)" file in the amends$`, haveAFileInTheAmends)
-			ctx.Step(`^"([^"]*)" have a "([^"]*)" url in the amends$`, haveAUrlInTheAmends)
 			ctx.Step(`^it does not have a config amends line on top of the file$`, itDoesNotHaveAConfigAmendsLineOnTopOfTheFile)
-			ctx.Step(`^it does not have an workflow amends line on top of the file$`, itDoesNotHaveAnWorkflowAmendsLineOnTopOfTheFile)
 			ctx.Step(`^it have a "([^"]*)" amends url line on top of the file$`, itHaveAAmendsUrlLineOnTopOfTheFile)
 			ctx.Step(`^it have a config amends line on top of the file$`, itHaveAConfigAmendsLineOnTopOfTheFile)
-			ctx.Step(`^it have a other amends line on top of the file$`, itHaveAOtherAmendsLineOnTopOfTheFile)
-			ctx.Step(`^it have a workflow amends line on top of the file$`, itHaveAWorkflowAmendsLineOnTopOfTheFile)
-			ctx.Step(`^it is a invalid agent$`, itIsAInvalidAgent)
-			ctx.Step(`^it is a invalid configuration file$`, itIsAInvalidConfigurationFile)
-			ctx.Step(`^it is a valid agent$`, itIsAValidAgent)
-			ctx.Step(`^it is a valid agent with a warning$`, itIsAValidAgentWithAWarning)
-			ctx.Step(`^it is a valid agent without a warning$`, itIsAValidAgentWithoutAWarning)
-			ctx.Step(`^it is a valid configuration file$`, itIsAValidConfigurationFile)
-			ctx.Step(`^it is an invalid agent$`, itIsAnInvalidAgent)
+			ctx.Step(`^it is an invalid configuration file$`, itIsAnInvalidPklFile)
+			ctx.Step(`^it is a valid configuration file$`, itIsAValidPklFile)
 			ctx.Step(`^the current directory is "([^"]*)"$`, theCurrentDirectoryIs)
 			ctx.Step(`^the home directory is "([^"]*)"$`, theHomeDirectoryIs)
-			ctx.Step(`^the valid workflow does not have a resources$`, theValidWorkflowDoesNotHaveAResources)
-			ctx.Step(`^the valid workflow have the following <resources>$`, theValidWorkflowHaveTheFollowingResources)
+			// Workflow steps
+			ctx.Step(`^a file "([^"]*)" exists in the "([^"]*)"$`, aFileExistsInThe)
+			ctx.Step(`^an agent folder "([^"]*)" exists in the current directory$`, anAgentFolderExistsInTheCurrentDirectory)
+			ctx.Step(`^it is a valid agent$`, itIsAValidPklFile)
+			ctx.Step(`^it is an invalid agent$`, itIsAnInvalidPklFile)
+			ctx.Step(`^we have a blank workflow file$`, weHaveABlankFile)
+			ctx.Step(`^it does not have a workflow amends line on top of the file$`, itDoesNotHaveAWorkflowAmendsLineOnTopOfTheFile)
+			ctx.Step(`^it have a workflow amends line on top of the file$`, itHaveAWorkflowAmendsLineOnTopOfTheFile)
 		},
 		Options: &godog.Options{
 			Format:   "pretty",
@@ -87,7 +114,9 @@ func TestFeatures(t *testing.T) {
 	}
 }
 
-func weHaveABlankConfigFile() error {
+// Config tests
+
+func weHaveABlankFile() error {
 	doc = ""
 	return nil
 }
@@ -128,14 +157,6 @@ func aFileExistsInTheCurrentDirectory(arg1 string) error {
 	return nil
 }
 
-func aFileAlsoExistsInTheFolder(arg1, arg2 string) error {
-	return godog.ErrPending
-}
-
-func aFileExistsInTheFolder(arg1, arg2 string) error {
-	return godog.ErrPending
-}
-
 func aSystemConfigurationIsDefined() error {
 	env := &cfg.Environment{
 		Home:           homeDirPath,
@@ -157,42 +178,10 @@ func aSystemConfigurationIsDefined() error {
 	return nil
 }
 
-func allResourcesHaveAValidAmendsLine() error {
-	return godog.ErrPending
-}
-
-func allResourcesHaveAValidDomain() error {
-	return godog.ErrPending
-}
-
-func allResourcesHaveAnAmendsLine() error {
-	return godog.ErrPending
-}
-
-func anAgentFolderExistsInTheCurrentDirectory(arg1 string) error {
-	return godog.ErrPending
-}
-
-func doesNotHaveAnAmendsLine(arg1 string) error {
-	return godog.ErrPending
-}
-
-func haveAFileInTheAmends(arg1, arg2 string) error {
-	return godog.ErrPending
-}
-
-func haveAUrlInTheAmends(arg1, arg2 string) error {
-	return godog.ErrPending
-}
-
 func itDoesNotHaveAConfigAmendsLineOnTopOfTheFile() error {
 	doc = fmt.Sprintf("%s", configValues)
 
 	return nil
-}
-
-func itDoesNotHaveAnWorkflowAmendsLineOnTopOfTheFile() error {
-	return godog.ErrPending
 }
 
 func itHaveAAmendsUrlLineOnTopOfTheFile(arg1 string) error {
@@ -202,61 +191,63 @@ func itHaveAAmendsUrlLineOnTopOfTheFile(arg1 string) error {
 }
 
 func itHaveAConfigAmendsLineOnTopOfTheFile() error {
-	doc = fmt.Sprintf("%s\n%s", amendsLine, configValues)
+	doc = fmt.Sprintf("%s\n%s", configAmendsLine, configValues)
 
 	return nil
 }
 
-func itHaveAOtherAmendsLineOnTopOfTheFile() error {
-	return godog.ErrPending
-}
-
-func itHaveAWorkflowAmendsLineOnTopOfTheFile() error {
-	return godog.ErrPending
-}
-
-func itIsAInvalidAgent() error {
-	return godog.ErrPending
-}
-
-func itIsAInvalidConfigurationFile() error {
-	if err := ValidateAmendsLine(fileThatExist, schemaVersionFilePath); err != nil {
-		return nil
-	} else {
-		return errors.New("expected an error, but got none")
+func itIsAnInvalidPklFile() error {
+	if err := EnforcePklTemplateAmendsRules(testFs, fileThatExist, schemaVersionFilePath); err == nil {
+		return errors.New("expected an error, but got nil")
 	}
 
 	return nil
 }
 
-func itIsAValidAgent() error {
-	return godog.ErrPending
-}
+func itIsAValidPklFile() error {
+	if err := EnforcePklTemplateAmendsRules(testFs, fileThatExist, schemaVersionFilePath); err != nil {
+		return err
+	}
 
-func itIsAValidAgentWithAWarning() error {
-	return godog.ErrPending
-}
-
-func itIsAValidAgentWithoutAWarning() error {
-	return godog.ErrPending
-}
-
-func itIsAValidConfigurationFile() error {
-	if err := ValidateAmendsLine(fileThatExist, schemaVersionFilePath); err != nil {
+	if _, err := evaluator.EvalPkl(testFs, fileThatExist); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func itIsAnInvalidAgent() error {
-	return godog.ErrPending
+// Workflow tests
+
+func aFileExistsInThe(arg1, arg2 string) error {
+	file := filepath.Join(agentPath, arg1)
+	fmt.Println(file)
+	f, _ := testFs.Create(file)
+	f.WriteString(doc)
+	f.Close()
+
+	fileThatExist = file
+
+	return nil
 }
 
-func theValidWorkflowDoesNotHaveAResources() error {
-	return godog.ErrPending
+func anAgentFolderExistsInTheCurrentDirectory(arg1 string) error {
+	agentPath = currentDirPath + "/my-agent"
+	if err := testFs.MkdirAll(agentPath, 0755); err != nil {
+		return err
+	}
+	fmt.Printf("Agent path %s created!", agentPath)
+
+	return nil
 }
 
-func theValidWorkflowHaveTheFollowingResources(arg1 *godog.Table) error {
-	return godog.ErrPending
+func itDoesNotHaveAWorkflowAmendsLineOnTopOfTheFile() error {
+	doc = fmt.Sprintf("%s", workflowValues)
+
+	return nil
+}
+
+func itHaveAWorkflowAmendsLineOnTopOfTheFile() error {
+	doc = fmt.Sprintf("%s\n%s", workflowAmendsLine, workflowValues)
+
+	return nil
 }
