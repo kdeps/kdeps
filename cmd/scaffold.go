9e3440d958b2f69136cceb0b50c07bc23dabcca3
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-01-02 12:08:39 +0100

    add ability to scaffold multiple items in a single invocation

--- cmd/scaffold.go
+++ cmd/scaffold.go
@@ -12,21 +12,20 @@ import (
 // NewScaffoldCommand creates the 'scaffold' subcommand for generating specific agent files
 func NewScaffoldCommand(fs afero.Fs, logger *log.Logger) *cobra.Command {
 	return &cobra.Command{
-		Use:   "scaffold [agentName] [fileName]",
-		Short: "Scaffold a specific file for an agent",
-		Args:  cobra.MaximumNArgs(2), // Allow up to two arguments (agentName, fileName)
+		Use:   "scaffold [agentName] [fileNames...]",
+		Short: "Scaffold specific files for an agent",
+		Args:  cobra.MinimumNArgs(2), // Require at least two arguments (agentName and at least one fileName)
 		Run: func(cmd *cobra.Command, args []string) {
-			var agentName, fileName string
-			if len(args) > 0 {
-				agentName = args[0]
-			}
-			if len(args) > 1 {
-				fileName = args[1]
-			}
+			agentName := args[0]
+			fileNames := args[1:]
 
-			if err := template.GenerateSpecificAgentFile(fs, logger, agentName, fileName); err != nil {
-				logger.Error("Error scaffolding file: ", err)
-				fmt.Println("Error:", err)
+			for _, fileName := range fileNames {
+				if err := template.GenerateSpecificAgentFile(fs, logger, agentName, fileName); err != nil {
+					logger.Error("Error scaffolding file:", err)
+					fmt.Println("Error:", err)
+				} else {
+					fmt.Printf("Successfully scaffolded file: %s\n", fileName)
+				}
 			}
 		},
 	}
