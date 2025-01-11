e897a0e235a4f8b430c8e1b038655c36ce0802d8
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-01-11 14:55:43 +0100

    skip condition now accepts string'd booleans

--- pkg/utils/conditions.go
+++ pkg/utils/conditions.go
@@ -1,19 +1,38 @@
 package utils
 
-func ShouldSkip(conditions *[]bool) bool {
+import "strings"
+
+// ShouldSkip checks if any condition in the list is true or the string "true".
+func ShouldSkip(conditions *[]interface{}) bool {
 	for _, condition := range *conditions {
-		if condition {
-			return true // Skip if any condition is true
+		switch v := condition.(type) {
+		case bool:
+			if v {
+				return true // Skip if any condition is true
+			}
+		case string:
+			if strings.ToLower(v) == "true" {
+				return true // Skip if any condition is "true" (case-insensitive)
+			}
 		}
 	}
 	return false
 }
 
-// Function to check if all conditions in a preflight check are met
-func AllConditionsMet(conditions *[]bool) bool {
+// AllConditionsMet checks if all conditions in the list are true or the string "true".
+func AllConditionsMet(conditions *[]interface{}) bool {
 	for _, condition := range *conditions {
-		if !condition {
-			return false // Return false if any condition is not met
+		switch v := condition.(type) {
+		case bool:
+			if !v {
+				return false // Return false if any condition is false
+			}
+		case string:
+			if strings.ToLower(v) != "true" {
+				return false // Return false if any condition is not "true" (case-insensitive)
+			}
+		default:
+			return false // Return false for unsupported types
 		}
 	}
 	return true // All conditions met
