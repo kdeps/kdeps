50971fcd891264a7e27b0345d383010bf05f44b0
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-01-16 02:14:33 +0100

    don't use init function (gochecknoinits) on logger

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -2,59 +2,79 @@ package logging
 
 import (
 	"os"
+	"sync"
 
 	"github.com/charmbracelet/log"
 )
 
 // Logger instance
-var logger *log.Logger
+var (
+	logger *log.Logger
+	once   sync.Once
+)
 
-func init() {
-	// Create a logger with default settings
-	logger = log.New(os.Stderr)
+// CreateLogger sets up the logger. It must be called before using the logger.
+func CreateLogger() {
+	once.Do(func() {
+		// Create a logger with default settings
+		logger = log.New(os.Stderr)
 
-	// Check if DEBUG environment variable is set to 1
-	if os.Getenv("DEBUG") == "1" {
-		// Set log options only when DEBUG is enabled
-		logger = log.NewWithOptions(os.Stderr, log.Options{
-			ReportCaller:    true,
-			ReportTimestamp: true,
-			Prefix:          "kdeps",
-		})
+		// Check if DEBUG environment variable is set to 1
+		if os.Getenv("DEBUG") == "1" {
+			// Set log options only when DEBUG is enabled
+			logger = log.NewWithOptions(os.Stderr, log.Options{
+				ReportCaller:    true,
+				ReportTimestamp: true,
+				Prefix:          "kdeps",
+			})
 
-		logger.SetLevel(log.DebugLevel)
-	} else {
-		// Use InfoLevel for normal operation without special logging options
-		logger.SetLevel(log.InfoLevel)
-	}
+			logger.SetLevel(log.DebugLevel)
+		} else {
+			// Use InfoLevel for normal operation without special logging options
+			logger.SetLevel(log.InfoLevel)
+		}
+	})
 }
 
 // Debug logs debug messages if debug logging is enabled.
 func Debug(msg interface{}, keyvals ...interface{}) {
+	ensureInitialized()
 	logger.Debug(msg, keyvals...)
 }
 
 // Info logs informational messages.
 func Info(msg interface{}, keyvals ...interface{}) {
-	logger.Debug(msg, keyvals...)
+	ensureInitialized()
+	logger.Info(msg, keyvals...)
 }
 
 // Warn logs warning messages.
 func Warn(msg interface{}, keyvals ...interface{}) {
+	ensureInitialized()
 	logger.Warn(msg, keyvals...)
 }
 
 // Error logs error messages.
 func Error(msg interface{}, keyvals ...interface{}) {
+	ensureInitialized()
 	logger.Error(msg, keyvals...)
 }
 
 // Fatal logs a fatal message and exits the program.
 func Fatal(msg interface{}, keyvals ...interface{}) {
+	ensureInitialized()
 	logger.Fatal(msg, keyvals...)
 }
 
 // GetLogger returns the Logger instance
 func GetLogger() *log.Logger {
+	ensureInitialized()
 	return logger
+}
+
+// ensureInitialized ensures the logger is initialized before use.
+func ensureInitialized() {
+	if logger == nil {
+		CreateLogger()
+	}
 }
