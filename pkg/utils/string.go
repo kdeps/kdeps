f1f465af811b5a3c691743398262200a117eb577
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-01-29 05:14:37 +0100

    refactor docker cache to reduce code complexity

--- pkg/utils/string.go
+++ pkg/utils/string.go
@@ -3,3 +3,12 @@ package utils
 func StringPtr(s string) *string {
 	return &s
 }
+
+func ContainsString(slice []string, target string) bool {
+	for _, s := range slice {
+		if s == target {
+			return true
+		}
+	}
+	return false
+}
