bb82faf293c1eaef3eb53ca6517c3eea60646849
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-05-07 17:02:56 +0200

    added new resource loader that includes the new memory reader support

--- pkg/resolver/resources.go
+++ pkg/resolver/resources.go
@@ -1,12 +1,30 @@
 package resolver
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
-	"github.com/kdeps/kdeps/pkg/resource"
+	"github.com/apple/pkl-go/pkl"
+	pklExec "github.com/kdeps/schema/gen/exec"
+	pklHTTP "github.com/kdeps/schema/gen/http"
+	pklLLM "github.com/kdeps/schema/gen/llm"
+	pklPython "github.com/kdeps/schema/gen/python"
+	pklResource "github.com/kdeps/schema/gen/resource"
 	"github.com/spf13/afero"
+)
+
+// ResourceType defines the type of resource to load.
+type ResourceType string
+
+const (
+	ExecResource   ResourceType = "exec"
+	PythonResource ResourceType = "python"
+	LLMResource    ResourceType = "llm"
+	HTTPResource   ResourceType = "http"
+	Resource       ResourceType = "resource"
 )
 
 // LoadResourceEntries loads .pkl resource files from the resources directory.
@@ -67,9 +85,14 @@ func (dr *DependencyResolver) handleFileImports(path string) error {
 // processPklFile processes an individual .pkl file and updates dependencies.
 func (dr *DependencyResolver) processPklFile(file string) error {
 	// Load the resource file
-	pklRes, err := resource.LoadResource(dr.Context, file, dr.Logger)
+	res, err := dr.LoadResource(dr.Context, file, Resource)
 	if err != nil {
-		return fmt.Errorf("failed to load resource from .pkl file %s: %w", file, err)
+		return fmt.Errorf("failed to load PKL file: %w", err)
+	}
+
+	pklRes, ok := res.(*pklResource.Resource)
+	if !ok {
+		return errors.New("failed to cast pklRes to *pklLLM.Resource")
 	}
 
 	// Append the resource to the list of resources
@@ -86,4 +109,88 @@ func (dr *DependencyResolver) processPklFile(file string) error {
 	}
 
 	return nil
+}
+
+// LoadResource reads a resource file and returns the parsed resource object or an error.
+func (dr *DependencyResolver) LoadResource(ctx context.Context, resourceFile string, resourceType ResourceType) (interface{}, error) {
+	// Log additional info before reading the resource
+	dr.Logger.Debug("reading resource file", "resource-file", resourceFile, "resource-type", resourceType)
+
+	// Define an option function to configure EvaluatorOptions
+	opts := func(options *pkl.EvaluatorOptions) {
+		pkl.WithDefaultAllowedResources(options)
+		pkl.WithOsEnv(options)
+		pkl.WithDefaultAllowedModules(options)
+		pkl.WithDefaultCacheDir(options)
+		options.Logger = pkl.NoopLogger
+		options.ResourceReaders = []pkl.ResourceReader{
+			dr.MemoryReader,
+		}
+	}
+
+	// Create evaluator with custom options
+	evaluator, err := pkl.NewEvaluator(ctx, opts)
+	if err != nil {
+		dr.Logger.Error("error creating evaluator", "error", err)
+		return nil, fmt.Errorf("error creating evaluator: %w", err)
+	}
+	defer func() {
+		if cerr := evaluator.Close(); cerr != nil && err == nil {
+			err = cerr
+			dr.Logger.Error("error closing evaluator", "error", err)
+		}
+	}()
+
+	// Load the resource based on the resource type
+	source := pkl.FileSource(resourceFile)
+	switch resourceType {
+	case Resource:
+		res, err := pklResource.Load(ctx, evaluator, source)
+		if err != nil {
+			dr.Logger.Error("error reading resource file", "resource-file", resourceFile, "error", err)
+			return nil, fmt.Errorf("error reading resource file '%s': %w", resourceFile, err)
+		}
+		dr.Logger.Debug("successfully loaded resource", "resource-file", resourceFile)
+		return res, nil
+
+	case ExecResource:
+		res, err := pklExec.Load(ctx, evaluator, source)
+		if err != nil {
+			dr.Logger.Error("error reading exec resource file", "resource-file", resourceFile, "error", err)
+			return nil, fmt.Errorf("error reading exec resource file '%s': %w", resourceFile, err)
+		}
+		dr.Logger.Debug("successfully loaded exec resource", "resource-file", resourceFile)
+		return res, nil
+
+	case PythonResource:
+		res, err := pklPython.Load(ctx, evaluator, source)
+		if err != nil {
+			dr.Logger.Error("error reading python resource file", "resource-file", resourceFile, "error", err)
+			return nil, fmt.Errorf("error reading python resource file '%s': %w", resourceFile, err)
+		}
+		dr.Logger.Debug("successfully loaded python resource", "resource-file", resourceFile)
+		return res, nil
+
+	case LLMResource:
+		res, err := pklLLM.Load(ctx, evaluator, source)
+		if err != nil {
+			dr.Logger.Error("error reading llm resource file", "resource-file", resourceFile, "error", err)
+			return nil, fmt.Errorf("error reading llm resource file '%s': %w", resourceFile, err)
+		}
+		dr.Logger.Debug("successfully loaded llm resource", "resource-file", resourceFile)
+		return res, nil
+
+	case HTTPResource:
+		res, err := pklHTTP.Load(ctx, evaluator, source)
+		if err != nil {
+			dr.Logger.Error("error reading http resource file", "resource-file", resourceFile, "error", err)
+			return nil, fmt.Errorf("error reading http resource file '%s': %w", resourceFile, err)
+		}
+		dr.Logger.Debug("successfully loaded http resource", "resource-file", resourceFile)
+		return res, nil
+
+	default:
+		dr.Logger.Error("unknown resource type", "resource-type", resourceType)
+		return nil, fmt.Errorf("unknown resource type: %s", resourceType)
+	}
 }
