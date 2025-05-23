6ed26508b41abf9f4395bdc3ce5e67a80876ddda
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-05-23 23:32:39 +0200

    update schema to 0.2.28

--- pkg/schema/schema.go
+++ pkg/schema/schema.go
@@ -12,7 +12,7 @@ import (
 var (
 	cachedVersion    string
 	once             sync.Once
-	specifiedVersion string = "0.2.27" // Default specified version
+	specifiedVersion string = "0.2.28" // Default specified version
 	UseLatest        bool   = false
 )
 
