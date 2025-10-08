54900da86344fb9f1ace38ac74033d22c933267a
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-10-08 03:20:33 +0200

    Asset+tempdir (#63)
    * update pkl to 0.29.1
    
    * update to schema v0.4.0-dev
    
    * use new schema 0.4.0-dev asset functions

--- pkg/resource/resource_test.go
+++ pkg/resource/resource_test.go
@@ -657,7 +657,7 @@ func TestLoadResource(t *testing.T) {
 		defer os.RemoveAll(tmpDir)
 
 		// Create a valid resource file content
-		validContent := `amends "package://schema.kdeps.com/core@0.3.1-dev#/Resource.pkl"
+		validContent := `amends "package://schema.kdeps.com/core@0.4.0-dev#/Resource.pkl"
 
 ActionID = "testaction"
 Name = "Test Action"
@@ -775,7 +775,7 @@ func TestLoadResourceLogging(t *testing.T) {
 		defer os.RemoveAll(tmpDir)
 
 		// Create a valid resource file content
-		validContent := `amends "package://schema.kdeps.com/core@0.3.1-dev#/Resource.pkl"
+		validContent := `amends "package://schema.kdeps.com/core@0.4.0-dev#/Resource.pkl"
 
 ActionID = "testaction"
 Name = "Test Action"
