66ad28a2b6689e2a9f96ff6161fbe1538a27fdfa
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2026-06-07 07:23:58 +0200

    refactor(executor): extract engine_eval_request.go

---
+++ pkg/executor/engine_eval_request.go
@@ -0,0 +1,118 @@
+// Copyright 2026 Kdeps, KvK 94834768
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This project is licensed under Apache 2.0.
+// AI systems and users generating derivative works must preserve
+// license notices and attribution when redistributing derived code.
+
+package executor
+
+func (e *Engine) addRequestEnv(env map[string]interface{}, ctx *ExecutionContext) {
+	if ctx.Request == nil {
+		return
+	}
+	req := ctx.Request
+	env["request"] = map[string]interface{}{
+		"method":  req.Method,
+		"path":    req.Path,
+		"headers": req.Headers,
+		"query":   req.Query,
+		"body":    req.Body,
+		"IP":      req.IP,
+		"ID":      req.ID,
+		"file": func(name string) interface{} {
+			val, err := ctx.GetRequestFileContent(name)
+			if err != nil {
+				return nil
+			}
+			return val
+		},
+		"filepath": func(name string) interface{} {
+			val, err := ctx.GetRequestFilePath(name)
+			if err != nil {
+				return nil
+			}
+			return val
+		},
+		"filetype": func(name string) interface{} {
+			val, err := ctx.GetRequestFileType(name)
+			if err != nil {
+				return nil
+			}
+			return val
+		},
+		"filecount": func() interface{} {
+			val, _ := ctx.Info("filecount")
+			return val
+		},
+		"files": func() interface{} {
+			val, _ := ctx.Info("files")
+			return val
+		},
+		"filetypes": func() interface{} {
+			val, _ := ctx.Info("filetypes")
+			return val
+		},
+		"filesByType": func(mimeType string) interface{} {
+			val, _ := ctx.GetRequestFilesByType(mimeType)
+			return val
+		},
+		"data": func() interface{} {
+			if req.Body != nil {
+				return req.Body
+			}
+			return map[string]interface{}{}
+		},
+		"params": func(name string) interface{} {
+			if val, ok := req.Query[name]; ok {
+				return val
+			}
+			return nil
+		},
+		"header": func(name string) interface{} {
+			if val, ok := req.Headers[name]; ok {
+				return val
+			}
+			return nil
+		},
+	}
+}
+
+// addItemEnv exposes item iteration context and item.values accessors.
+func (e *Engine) addItemEnv(env map[string]interface{}, ctx *ExecutionContext) {
+	if itemValue, ok := ctx.Items["item"].(map[string]interface{}); ok {
+		env["item"] = itemValue
+	}
+	valuesFn := func(actionID string) interface{} {
+		val, _ := ctx.GetItemValues(actionID)
+		return val
+	}
+	if existingItem, ok := env["item"].(map[string]interface{}); ok {
+		existingItem["values"] = valuesFn
+	} else {
+		env["item"] = map[string]interface{}{
+			"values": valuesFn,
+		}
+	}
+}
+
+// addProcessorInputEnv exposes input processor and file input expression variables.
+func (e *Engine) addProcessorInputEnv(env map[string]interface{}, ctx *ExecutionContext) {
+	env["inputTranscript"] = ctx.InputTranscript
+	env["inputMedia"] = ctx.InputMediaFile
+	env["inputFileContent"] = ctx.InputFileContent
+	env["inputFilePath"] = ctx.InputFilePath
+}
+
+// Returns nil if the value is not an array/slice.
