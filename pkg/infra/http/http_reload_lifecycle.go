7f46ff49b66b5d358ee24c3a61f98667c0ce0fe4
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2026-06-09 05:32:23 +0200

    refactor: split remaining pkg/infra/http micro-modules
    - Split http_trusted_proxy into settings and IP parsing modules
    - Extract map constructors and workflow metadata from http_conditions
    - Merge server_reload into http_reload_lifecycle
    - Remove clientIPFromAddr alias; tests use peerIPFromAddr directly

--- pkg/infra/http/http_reload_lifecycle.go
+++ pkg/infra/http/http_reload_lifecycle.go
@@ -21,6 +21,11 @@ package http
 import (
 	"errors"
 	"log/slog"
+
+	"github.com/kdeps/kdeps/v2/pkg/parser/expression"
+	"github.com/kdeps/kdeps/v2/pkg/parser/yaml"
+	"github.com/kdeps/kdeps/v2/pkg/templates"
+	"github.com/kdeps/kdeps/v2/pkg/validator"
 )
 
 func requireWatcher(watcher FileWatcher) error {
@@ -95,4 +100,113 @@ func (s *Server) rebuildRouterPreservingMiddleware() {
 	s.Router = NewRouter()
 	s.Router.Middleware = oldMiddleware
 	s.SetupRoutes()
+}
+
+func (s *Server) SetupHotReload() error {
+	debugEnter("SetupHotReload")
+	if err := requireWatcher(s.Watcher); err != nil {
+		return err
+	}
+
+	watchWorkflowPath := s.hotReloadWorkflowPath()
+
+	absWorkflowPath := absWorkflowPathOrRelative(watchWorkflowPath, s.logger)
+
+	if parserErr := s.ensureWorkflowParser(); parserErr != nil {
+		return parserErr
+	}
+
+	reloadOnChange := s.hotReloadCallback()
+
+	workflowChanged := reloadOnChange(hotReloadWorkflowChangeMessage)
+	if watchErr := s.Watcher.Watch(absWorkflowPath, workflowChanged); watchErr != nil {
+		return hotReloadWatchWorkflowFailed(watchErr)
+	}
+
+	resourcesPath := workflowResourcesDir(absWorkflowPath)
+	resourcesChanged := reloadOnChange(hotReloadResourcesChangeMessage)
+	s.watchOptionalResourcesDir(resourcesPath, resourcesChanged)
+
+	return nil
+}
+
+func (s *Server) hotReloadWorkflowPath() string {
+	if path := s.lockedWorkflowPath(); path != "" {
+		return path
+	}
+	return defaultWorkflowFile
+}
+
+func (s *Server) watchOptionalResourcesDir(path string, onChange func()) {
+	if watchErr := s.Watcher.Watch(path, onChange); watchErr != nil {
+		logOptionalWatchFailure(s.logger, path, watchErr)
+	}
+}
+
+func (s *Server) reloadWorkflow() error {
+	debugEnter("reloadWorkflow")
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.ensureReloadReady(); err != nil {
+		return err
+	}
+
+	if prepErr := templates.PreprocessJ2Files(workflowDirFromPath(s.workflowPath)); prepErr != nil {
+		return hotReloadPreprocessFailed(prepErr)
+	}
+
+	newWorkflow, err := s.parser.ParseWorkflow(s.workflowPath)
+	if err != nil {
+		return hotReloadParseFailed(err)
+	}
+
+	s.Workflow = newWorkflow
+	s.rebuildRouterPreservingMiddleware()
+	logReloadedWorkflow(s)
+
+	return nil
+}
+
+func (s *Server) ensureWorkflowParser() error {
+	if hasWorkflowParser(s.parser) {
+		return nil
+	}
+	parser, err := workflowParserFactory()
+	if err != nil {
+		return err
+	}
+	s.parser = parser
+	return nil
+}
+
+func (s *Server) ensureReloadReady() error {
+	if err := s.ensureWorkflowParser(); err != nil {
+		return err
+	}
+
+	if s.workflowPath != "" {
+		return nil
+	}
+
+	absPath, absErr := filepathAbs(resolveDefaultWorkflowPath())
+	if absErr != nil {
+		return hotReloadResolvePathFailed(absErr)
+	}
+	s.workflowPath = absPath
+	return nil
+}
+
+//nolint:gochecknoglobals // test-replaceable
+var (
+	workflowParserFactory  = newWorkflowParser
+	schemaValidatorFactory = validator.NewSchemaValidator
+)
+
+func newWorkflowParser() (*yaml.Parser, error) {
+	schemaValidator, schemaErr := schemaValidatorFactory()
+	if schemaErr != nil {
+		return nil, workflowParserSchemaValidatorFailed(schemaErr)
+	}
+	return yaml.NewParser(schemaValidator, expression.NewParser()), nil
 }
